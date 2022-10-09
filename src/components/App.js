66f0bed65d0bb0fd2e593d17e1cc14672d7efc05
Yugesh Kushwaha <96573577+yugesh01@users.noreply.github.com>
2022-10-09 15:35:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
+// import axios from 'axios';
+
 const App = () => {
+ 
+ const [name, setName]= useState("");
+ const [data , setData]=useState([]);
+ 
+ useEffect(()=>{
+  fetch("https://www.content.newtonschool.co/v1/main/users")
+   .then((res)=>res.json())
+   .then(({users})=> setName(users));
+ });
+ 
+//   axios.get(`
+  
+ const changeInput=(e)=>{
+  setData(e.target.value);
+  
+ }
 //code here 
  
 
